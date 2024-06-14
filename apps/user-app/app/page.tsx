02abc1005004ff472a0fff3262193f23ef30eb28
Madhav Sahi <63411260+MadhavSahi@users.noreply.github.com>
2024-06-14 20:44:20 +0530

    Update page.tsx

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -6,8 +6,10 @@ import { Appbar } from "@repo/ui/appbar";
 export default function Page(): JSX.Element {
   const session = useSession();
   return (
+   <>
    <div>
       <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
    </div>
+<>
   );
 }
